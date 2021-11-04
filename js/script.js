05241328e2fec4f222b79bbe10653c63067176e5
Igor <zaplatynskyy@gmail.com>
2021-11-04 15:40:11 +0100

    prompt dei dati e console.log risultato

--- js/script.js
+++ js/script.js
@@ -4,5 +4,14 @@ let cognome;
 let colorePreferito;
 const numero = 21;
 
+// sezione metodi prompt
 nome = prompt('Inserisci il tuo nome');
-console.log(nome);
+cognome = prompt('Inserisci il tuo cognome');
+colorePreferito = prompt('Inserisci il tuo colore preferito');
+
+// sezione console.log
+console.log('Il nome inserito è ' + nome);
+console.log('Il cognome inserito è ' + cognome);
+console.log('Il colore preferito inserito è ' + colorePreferito);
+console.log('La tua nuova password è ' + nome + cognome + colorePreferito + numero);
+
