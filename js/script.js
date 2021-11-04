753e647c543c31036aacb330d52f885fb32e0a13
Igor <zaplatynskyy@gmail.com>
2021-11-04 15:57:44 +0100

    stampa risultato sul browser

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
-// dichiarazione variabili
 let nome;
 let cognome;
 let colorePreferito;
 const numero = 21;
+let password;
 
-// sezione metodi prompt
 nome = prompt('Inserisci il tuo nome');
 cognome = prompt('Inserisci il tuo cognome');
 colorePreferito = prompt('Inserisci il tuo colore preferito');
 
-// sezione console.log
-console.log('Il nome inserito è ' + nome);
-console.log('Il cognome inserito è ' + cognome);
-console.log('Il colore preferito inserito è ' + colorePreferito);
-console.log(
-    `La tua nuova password è ${nome}${cognome}${colorePreferito}${numero}`
-);
+password = nome + cognome + colorePreferito + numero;
+console.log('La tua nuova password è ' + password);
+
+document.getElementById('nome').innerHTML = nome;
+document.getElementById('password').innerHTML = password;
+
+// console.log('Il nome inserito è ' + nome);
+// console.log('Il cognome inserito è ' + cognome);
+// console.log('Il colore preferito inserito è ' + colorePreferito);
+// console.log(
+//     `La tua nuova password è ${nome}${cognome}${colorePreferito}${numero}`
+// );
 
